7eb95345e7c3bb3d10525861b62b5e997abcca6c
goga133 <asromanyuk@edu.hse.ru>
2021-02-07 21:39:08 +0300

    add 04-1 task

---
+++ lab3/04-1.c
@@ -0,0 +1,27 @@
+// Задача №3.
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[]){
+    printf("Количество аргументов командной строки: %d\n\r", argc);
+    printf("Аргументы командной строки: ");
+
+    for (int i = 0; i < argc; i++)
+    {
+        if(i != argc - 1){
+            printf("%s, ", argv[i]);
+        }
+        else{
+            printf("%s.\n\r", argv[i]);
+        }
+    }
+    
+    printf("Параметры окружающей среды:\n");
+    int i = 0;
+    while (envp[i] != NULL){
+        printf("%s\n", envp[i++]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
