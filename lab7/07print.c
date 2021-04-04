7460029811465bcbd6c98e1e93e725f10d922329
goga133 <asromanyuk@edu.hse.ru>
2021-04-04 22:00:10 +0300

    Lab 8 done

---
+++ lab7/07print.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <errno.h>
+
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include "stdlib.h"
+
+int main()
+{
+    int shmid;
+    key_t key;
+
+    char pathname[] = "07print.c";
+    if ((key = ftok(pathname,0)) < 0) {
+        printf("Can\'t generate key\n");
+        exit(-1);
+    }
+
+    int file_size = 0;
+
+    // Взято с примера
+    if ((shmid = shmget(key, sizeof(int) + file_size * sizeof(char), 0666|IPC_CREAT)) < 0) {
+        printf("Can\'t create shared memory\n");
+        exit(-1);
+    }
+
+    int *file_size_ptr;
+
+    if ((file_size_ptr = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
+        printf("Can't attach shared memory\n");
+        exit(-1);
+    }
+
+    file_size = *file_size_ptr;
+    char *file_text;
+    file_text = (char*)(file_size_ptr + 1);
+    
+    for (int i = 0; i < file_size; i++){
+        putchar(file_text[i]);
+    }
+        
+    if (shmdt(file_size_ptr) < 0) {
+        printf("Can't detach shared memory\n");
+        exit(-1);
+    }
+    
+    if (shmctl(shmid, IPC_RMID, NULL) < 0) {
+        printf("Can't delete shared memory\n");
+        exit(-1);
+    }
+    return 0;
+}
