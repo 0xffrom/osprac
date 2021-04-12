3499963307ca36cb0567f7a2c28aadf1d274377b
Andrey Romanyuk <asromanyuk@edu.hse.ru>
2021-04-12 17:21:21 +0300

    Update 07-3a.c

--- lab9/07-3a.c
+++ lab9/07-3a.c
@@ -6,23 +6,6 @@
 #include <stdlib.h>
 #include <sys/sem.h>
 
-int incr(int semid, struct sembuf* buf) {
-    buf->sem_flg = 0;
-    buf->sem_num = 0;
-    buf->sem_op = 1;
-
-    return semop(semid, buf, 1);
-}
-
-int decr(int semid, struct sembuf* buf) {
-    buf->sem_flg = 0;
-    buf->sem_op = -1;
-    buf->sem_num = 0;
-
-    return semop(semid, buf, 1);
-}
-
-
 int main()
 {
   struct sembuf mybuf;
@@ -38,53 +21,75 @@ int main()
     printf("Can\'t generate key\n");
     exit(-1);
   }
+    
+  addbuf.sem_op = 1;
+  addbuf.sem_num = 0;
+  addbuf.sem_flg = 0;
+
+  decbuf.sem_op = -1;
+  decbuf.sem_num = 0;
+  decbuf.sem_flg = 0;
+    
+  if ((semid = semget(key, 1, 0666)) < 0) {
+    printf("Semaphore doesnt exist\n");
+    if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+      printf("Cant create new semaphore");
+      exit(-1);
+    }
+    printf("Semaphore was created\n");
+    if (semop(semid, &addbuf, 1) < 0) {
+      printf("Cant wait for condition\n");
+      exit(-1);
+    }
+  }
 
   if ((shmid = shmget(key, 3*sizeof(int), 0666|IPC_CREAT|IPC_EXCL)) < 0) {
     if (errno != EEXIST) {
-      printf("Can\'t create shared memory\n");
+      printf("Cant create shared memory\n");
       exit(-1);
     } else {
       if ((shmid = shmget(key, 3*sizeof(int), 0)) < 0) {
-        printf("Can\'t find shared memory\n");
+        printf("Cant find shared memory\n");
         exit(-1);
       }
       new = 0;
     }
   }
-
+    
   if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
     printf("Can't attach shared memory\n");
     exit(-1);
   }
 
-  if ((semid = semget(key, 1, 0666)) < 0) {
-      printf("Семафор не найден, повтор¤ю попытку с флагом IPC CREAT...\n");
-      if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-          printf("Создать невозможно, аварийно завершаюсь.\n");
-          exit(-1);
-      }
-      printf("Успешно создан семафор!\n");
-      incr(semid, &mybuf);
+  //Уменьшаем семафор на 1 тем самым не давая
+  //второму процессу изменить хранимые в
+  //разделенной памяти данные
+  if (semop(semid, &decbuf, 1) < 0) {
+    printf("Can\'t wait for condition\n");
+    exit(-1);
   }
-
+    
   if (new) {
     array[0] =  1;
     array[1] =  0;
     array[2] =  1;
   } else {
-    decr(semid, &mybuf);
     array[0] += 1;
     for(i=0; i<2000000000L; i++);
     array[2] += 1;
-    incr(semid, &mybuf);
   }
 
   printf
     ("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
     array[0], array[1], array[2]);
 
+  if (semop(semid, &addbuf, 1) < 0) {
+ 		printf("Cant wait for condition\n");
+   	exit(-1);
+  }
+    
   if (shmdt(array) < 0) {
-    printf("Can't detach shared memory\n");
+    printf("Cant detach shared memory\n");
     exit(-1);
   }
 
