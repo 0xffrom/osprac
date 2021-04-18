5f975c01828bd94bd6962cc3679c729200cba875
Andrew <andrewkpyt@yandex.ru>
2021-04-18 22:30:24 +0300

    lab 10 done?

---
+++ lab10/05-3.c
@@ -0,0 +1,125 @@
+#include <time.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/sem.h>
+#include <stdlib.h>
+
+int inc(int semid, struct sembuf* buf) {
+    buf->sem_op = 1;
+    buf->sem_flg = 0;
+    buf->sem_num = 0;
+
+    return semop(semid, buf, 1);
+}
+
+int dec(int semid, struct sembuf* buf) {
+    buf->sem_op = -1;
+    buf->sem_flg = 0;
+    buf->sem_num = 0;
+
+    return semop(semid, buf, 1);
+}
+
+int main()
+{
+    int parent[2], result;
+    size_t size = 0;
+    key_t key;
+    char pathname[] = "05-3.c";
+    struct sembuf mybuf;
+
+    int N;
+    printf("Введите N: \n");
+    scanf("%d", &N);
+    char resstring[14 * N];
+
+    if (N <= 2) {
+        printf("N должен быть больше 2.\n");
+        exit(-1);
+    }
+
+    // Создаём Pipes родителя и ребёнка:
+    if (pipe(parent) < 0) {
+        printf("Невозможно создать pipe отца\n\r");
+        exit(-1);
+    }
+
+    if ((key = ftok(pathname, 0)) < 0) {
+        printf("Ключ не сгенерирован.\n");
+        exit(-1);
+    }
+
+    // Создаём семафорчик
+    if ((semid = semget(key, 1, 0666)) < 0) {
+        printf("Семафор не найден, повторяю: \n");
+        if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+            printf("Не могу получить Semid\n");
+            exit(-1);
+        }
+        printf("Успешно создан!\n");
+    }
+
+    result = fork();
+
+    if (result < 0) {
+        printf("Неудачный fork\n\r");
+        exit(-1);
+    }
+
+    else if (result > 0) {
+        for (int i = 0; i < N; ++i) {
+            size += write(parent[1], "Hello, world!", 14);
+        }
+        
+        // P.s обработка ошибок как в примере.
+        if (size != 14 * N) {
+            printf("Can\'t write all string\n\r");
+            exit(-1);
+        }
+        
+        // Ждём ребёнка:
+        dec(semid, &mybuf); 
+
+        // Читаем ребёнка:
+        size = read(child[0], resstring, 14 * N);
+        if (size != 14 * N) {
+            printf("Can\'t read from child\n\r");
+            exit(-1);
+        }
+
+        close(child[0]);
+        printf("Отец прочитал с ребёнка: %s\n\rОтец закончил.\n\r", resstring);
+    }
+    else {
+        // Ребёнок
+
+        // Читаем отца:
+        size = read(parent[0], resstring, 14 * N);
+        if (size < 0) {
+            printf("Can\'t get from parent\n\r");
+            exit(-1);
+        }
+        // Печатаем, что прочитали:
+        printf("Ребёнок получил с отца: %s\n\r", resstring);
+        // Запишем ребёнком, что прочитали:
+        size = write(parent[1], resstring, 14 * N);
+
+        // Отцом:
+        inc(semid, &mybuf); 
+
+        if (size != 14 * N) {
+            printf("Невозможно написать всю строку.\n");
+            exit(-1);
+        }
+        else {
+            printf("Всё тип топ.\n");
+        }
+
+        close(parent[1]);
+        close(parent[0]);
+        printf("Ребёнок вышел\n\r");
+    }
+    return 0;
+}
